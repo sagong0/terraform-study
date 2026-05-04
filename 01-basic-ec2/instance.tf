dd3577276caf10d58cead0ad04763afa6ee130b7
sagong0 <1w2q3e@naver.com>
2026-05-04 20:34:15 +0900

    aws_instance attribute - subnet_id add

--- 01-basic-ec2/instance.tf
+++ 01-basic-ec2/instance.tf
@@ -7,6 +7,8 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type.example1
 
+  subnet_id = module.vpc.public_subnets[0]
+
   tags = {
     Name="example"
   }
